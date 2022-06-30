66bccd119eb0a341fac44a871c8c76cb3dbbf9ea
Matt <mgstankov30@gmail.com>
2022-06-30 19:44:29 -0400

    add functionality for game file in two player game

--- game.rb
+++ game.rb
@@ -2,32 +2,39 @@ class Game
   def initialize
     @player1 = Player.new("Player1")
     @player2 = Player.new("Player2")
+    @user_turn = @player1
+    @is_game_over = false
   end
 
 def start
-  puts "New Game Stirbarted. Welcome #{player1.name} and #{player2.name}"
+  puts "New Game Started. Welcome #{@player1.name} and #{@player2.name}"
   turn
 end
 
 def next_question 
+  puts "-----Next Turn-----"
   question = Question.new
   question.ask_question
-  question.check_answer
   if question.check_answer
     puts "Yes! You are correct!"
   else
     puts "Seriously? No!"
-    take_life
+    @user_turn.take_life
+  end
+  if @user_turn.name == @player1.name
+  @user_turn = @player2
+  else 
+    @user_turn = @player1
   end
 end
 
 def check_standing
   if @player1.is_dead?
+    @is_game_over = true
     @player2.winner
+  elsif @player2.is_dead?
     @is_game_over = true
-  elsif @player2.is_dead
     @player1.winner
-    @is_game_over = true
   end
 end
 
@@ -35,18 +42,11 @@ def show_lives
   puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
 end  
 
-def winner
-  puts "#{Player.name} wins with the score of #{Player.lives}/3"
-end
-
-def turn (is_game_over)
-  while is_game_over == false
-    @player1.next_question
-    check_standing
-    @player2.next_question
+def turn
+  while @is_game_over == false
+    next_question
     check_standing
     show_lives
-    puts "Next Turn"
     turn
   end  
 end
