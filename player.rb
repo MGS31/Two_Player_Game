d74f558253235e3e12f67f81cf38dc4cc3727cd7
Matt <mgstankov30@gmail.com>
2022-06-30 19:44:38 -0400

    add player details to the file

--- player.rb
+++ player.rb
@@ -1,16 +1,21 @@
 class Player
+  attr_accessor :name, :lives
+  
   def initialize(name)
     @lives = 3
     @name = name
   end  
 
 def is_dead? 
-  # puts "game over" if
    @lives == 0
 end
 
 def take_life
   @lives -= 1
+end
+
+def winner
+  print "#{@name} wins with the score of #{@lives}/3\n"
 end
 
 end 
