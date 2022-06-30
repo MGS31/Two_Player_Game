317f0a9b5b7d4b811f15076392a5107ba1bb687e
Matt <mgstankov30@gmail.com>
2022-06-30 19:44:58 -0400

    add funcionality for the questions of the two player game

--- question.rb
+++ question.rb
@@ -1,17 +1,17 @@
 class Question 
   def initialize
-    @num1 = ran(1..20)
-    @num2 = ran(1..20)
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
     @sum = @num1 + @num2
   end
 
   def check_answer
-    answer = gets.chomp
+    answer = gets.chomp.to_i
     @sum == answer
   end
 
   def ask_question
-    puts "#{Player.name} What does #{num1} plus #{num2} equal?"
+    puts "What does #{@num1} plus #{@num2} equal?"
   end  
   
 end 
